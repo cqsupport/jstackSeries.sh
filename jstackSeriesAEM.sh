2e6e332c8fb06797518803d3b48e9e973d6ce7fc
Andrew Khoury <andrewmkhoury@gmail.com>
2018-04-05 13:45:24 -0700

    Update jstackSeriesAEM.sh

--- jstackSeriesAEM.sh
+++ jstackSeriesAEM.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 # Adaptation of script from eclipse.org http://wiki.eclipse.org/How_to_report_a_deadlock#jstackSeries_--_jstack_sampling_in_fixed_time_intervals_.28tested_on_Linux.29
-export JAVA_HOME=/usr/java/jdk1.8.0_121/bin/
-export AEM_HOME=/opt/aem/publish62
+# Define these two variables
+JAVA_HOME=/usr/java/jdk1.8.0_102
+AEM_HOME=/mnt/crx/author
 
-pid=${1:-$(cat $AEM_HOME/crx-quickstart/conf/cq.pid)} # required
+JAVA_HOME=${JAVA_HOME:+${JAVA_HOME%/}/}
+JAVA_BIN=${JAVA_HOME}bin/
+AEM_HOME=${AEM_HOME:+${AEM_HOME%/}/}
+AEM_JAR=$(ls -1 /mnt/crx/author | grep -E "author|publish|cq|aem.*\.jar$" | grep -v -E "oak|crx" | head -1)
+
+echo "cat ${AEM_HOME}crx-quickstart/conf/cq.pid"
+
+# Retrieve pid from the cq.pid file
+pid=$(cat ${AEM_HOME}crx-quickstart/conf/cq.pid)
+# If cq.pid file doesn't exist then fail over to grepping for process that has jar file name
+pid=${pid:-$(ps aux | grep $AEM_JAR | grep -v grep | awk '{print $2}' | head -1)}
+
+
 if [ -z "$pid" ]; then
    echo >&2 "Error: Missing PID"
-   echo >&2 "Usage: jstackSeriesAEM.sh <pid> [ <count> [ <delay> ] ]"
-   echo >&2 "    Defaults: pid=$(cat $AEM_HOME/crx-quickstart/conf/cq.pid), count = 10, delay = 1 (seconds)"
+   echo >&2 "Usage: jstackSeriesAEM.sh [ <count> [ <delay> ] ]"
+   echo >&2 "    Defaults: count = 10, delay = 1 (seconds)"
    exit 1
 fi
-count=${2:-10}  # defaults to 10 times
-delay=${3:-1} # defaults to 1 second
+
+
+count=${1:-10}  # defaults to 10 times
+delay=${2:-1} # defaults to 1 second
 echo "Running with params - PID: $pid, Count: $count, Delay: $delay"
+DUMP_DIR=${AEM_HOME}crx-quickstart/logs/threaddumps/$pid.$(date +%s.%N)
+mkdir -p $DUMP_DIR
+DUMP_DIR=${DUMP_DIR:+${DUMP_DIR%/}/}
+echo "Generating files under ${DUMP_DIR}"
 while [ $count -gt 0 ]
 do
-    ${JAVA_HOME}jstack $pid >jstack.$pid.$(date +%s.%N)
-    top -H -b -n1 -p $pid >top.$pid.$(date +%s.%N)
+    ${JAVA_BIN}jstack $pid > ${DUMP_DIR}jstack.$pid.$(date +%s.%N)
+    top -H -b -n1 -p $pid > ${DUMP_DIR}top.$pid.$(date +%s.%N)
     sleep $delay
     let count--
     echo -n "."
